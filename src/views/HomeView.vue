79186603babf319f5b734799f5eb8f29cbf1c80a
Sourav Soni <sauravsoni8980@gmail.com>
2024-08-09 11:49:27 +0530

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import Joblistings from "@/components/JobListings.vue";
+</script>
+
+<template>
+    <Hero />
+    <HomeCards />
+    <Joblistings :limit="3" :showButton="true" />
+</template>
